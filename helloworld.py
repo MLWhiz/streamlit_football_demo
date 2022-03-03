a7cd541a010ff33b8677a98151224985e6d9ed49
kushuu <sonikushu007@gmail.com>
2022-03-03 16:00:47 +0530

    added a new plot and a new filter field. cleaned the data a bit

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly_express as px
+import plotly.express as px
+
 
 '''
 # Club and Nationality App
@@ -12,15 +12,23 @@ df = st.cache(pd.read_csv)("football_data.csv")
 
 clubs = st.sidebar.multiselect('Show Player for clubs?', df['Club'].unique())
 nationalities = st.sidebar.multiselect('Show Player from Nationalities?', df['Nationality'].unique())
+work_rate = st.sidebar.multiselect('Show Player with work rate?', df['Work Rate'].unique())
 
-new_df = df[(df['Club'].isin(clubs)) & (df['Nationality'].isin(nationalities))]
+new_df = df[(df['Club'].isin(clubs)) & (df['Nationality'].isin(nationalities)) & (df['Work Rate'].isin(work_rate))]
+new_df.reset_index(drop = True, inplace = True)
 st.write(new_df)
 
 # Create distplot with custom bin_size
-fig = px.scatter(new_df, x ='Overall',y='Age',color='Name')
-
-'''
-### Here is a simple chart between player age and overall
-'''
-
-st.plotly_chart(fig)
+try:
+    fig = px.scatter(new_df, x ='Overall',y='Age',color='Name')
+    # fig2 = px.scatter_polar(new_df, r='SprintSpeed', theta='Acceleration', color = 'Name', template='plotly_dark')
+    # px.treemap(new_df, )
+    # px.scatter_ternary(new_df, )
+    fig2 = px.scatter_polar(new_df, r = 'SprintSpeed', theta='Acceleration', template='plotly_dark', color='Name')
+    '''
+    ### Here is a simple chart between player age and overall
+    '''
+    st.plotly_chart(fig)
+    st.plotly_chart(fig2)
+except KeyError as e:
+    print("\nNothing to show on the graph right now!\n")
